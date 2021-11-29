dc92488c57981682a4fc2cfa249a3e2f758d4112
Shahzeb Malik <33540322+ShahzebMalik107@users.noreply.github.com>
2021-11-29 19:45:38 +0500

    Database connected

---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const conectDB = async()=>{
+    try {
+        
+        await mongoose.connect(db, {
+            useNewUrlParser: true
+        });
+        console.log('Mongo DB connected')
+
+    } catch (err) {
+        console.error(err.message);
+        console.log(err);
+
+        process.exit(1);
+    }
+}
+
+module.exports = conectDB;
